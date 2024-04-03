ae292bc645d0d4520e854c0f2edc1dc779c1cf39
RafaelH <rafael0henkel1@gmail.com>
2024-04-02 22:02:54 -0300

    api finished

---
+++ js/script.js
@@ -0,0 +1,54 @@
+const createAccount = async (event) => {
+  event.preventDefault();
+  try {
+    const name = document.getElementById("name").value;
+    const mail = document.getElementById("email").value;
+    const pass = document.getElementById("password").value;
+
+    const data = {
+      name,
+      mail,
+      pass,
+    };
+
+    localStorage.setItem("register", JSON.stringify(data));
+
+    const dataSave = JSON.parse(localStorage.getItem("register"));
+
+    const response = await api.post("signup", dataSave);
+
+    console.log("conta criada", response.data);
+
+    localStorage.removeItem("register");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const login = async (event) => {
+  event.preventDefault();
+  try {
+    const mail = document.getElementById("email").value;
+    const pass = document.getElementById("password").value;
+
+    
+
+    const data = {
+      mail,
+      pass,
+    };
+
+    localStorage.setItem("login", JSON.stringify(data));
+
+    const dataSave = JSON.parse(localStorage.getItem("login"));
+
+    const response = await api.post("login", dataSave);
+
+    console.log("login realizado", response.data);
+
+    localStorage.removeItem("login");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
